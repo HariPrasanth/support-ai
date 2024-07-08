2d337c724fb6c9db006ab375029770c8ab537840
tech-admins <tech-admins@appsforbharat.com>
2024-07-08 13:52:38 +0530

    Code changes

--- models/save_model.py
+++ models/save_model.py
@@ -1,11 +1,3 @@
 import joblib
-from sentence_transformers import SentenceTransformer
 
-# Load a pre-trained model from Hugging Face
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-# Save the model to a file
-model_filename = 'models/sentence_transformer_model.pkl'
-joblib.dump(model, model_filename)
-
-print("Model saved locally.")
+print("joblib is installed and working correctly.")
